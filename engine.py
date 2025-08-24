539b4d7a1de5806223177d7884a53cff53c07893
JohannaG33 <johanna.guilhemjouan@gmail.com>
2025-08-24 22:40:11 +0200

    Part 8 - Inventory OK

--- engine.py
+++ engine.py
@@ -6,6 +6,7 @@ from tcod.context import Context
 from tcod.console import Console
 from tcod.map import compute_fov
 
+import exceptions
 from actions import EscapeAction, MovementAction
 from input_handlers import MainGameEventHandler
 from message_log import MessageLog
@@ -29,7 +30,10 @@ class Engine:
     def handle_enemy_turns(self) -> None:
         for entity in set(self.game_map.actors) - {self.player}:
             if entity.ai:
-                entity.ai.perform()
+                try:
+                    entity.ai.perform()
+                except exceptions.Impossible:
+                    pass  # Ignore impossible action exceptions from AI.
 
     def update_fov(self) -> None:
         """Recompute the visible area based on the players point of view."""
